b36823d90aa39570ef6ca81cd571d4d62bdadbcf
seungbeom22 <seungbeomseo18@gmail.com>
2026-05-24 12:28:37 +0900

    Refactor memory and display update functions

--- script.js
+++ script.js
@@ -7,28 +7,20 @@ function formatNumber(num) {
     return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 function unformatNumber(num) { return num.toString().replace(/,/g, ""); }
-function updateMemIndicator() { memIndicator.innerText = memoryValue !== 0 ? `M: ${formatNumber(memoryValue)}` : ""; }
 
 function memory(type) {
     let currentVal = parseFloat(unformatNumber(display.value)) || 0;
-    if(type === 'MC') { memoryValue = 0; }
-    else if(type === 'MR') { display.value = formatNumber(memoryValue); }
+    if(type === 'MC') memoryValue = 0;
+    else if(type === 'MR') display.value = formatNumber(memoryValue);
     else if(type === 'M+') { memoryValue += currentVal; clearDisplay(); }
     else if(type === 'M-') { memoryValue -= currentVal; clearDisplay(); }
-    updateMemIndicator();
+    memIndicator.innerText = memoryValue !== 0 ? `M: ${formatNumber(memoryValue)}` : "";
 }
 
-function appendToDisplay(value) {
-    let currentVal = unformatNumber(display.value);
-    display.value = formatNumber(currentVal + value);
-}
-
+function appendToDisplay(value) { display.value = formatNumber(unformatNumber(display.value) + value); }
 function clearDisplay() { display.value = ''; }
 function deleteLast() { display.value = formatNumber(unformatNumber(display.value).slice(0, -1)); }
-
 function calculate() {
-    try {
-        let expression = unformatNumber(display.value).replace(/×/g, '*').replace(/÷/g, '/');
-        display.value = formatNumber(eval(expression));
-    } catch { display.value = '오류'; }
+    try { display.value = formatNumber(eval(unformatNumber(display.value).replace(/×/g, '*').replace(/÷/g, '/'))); }
+    catch { display.value = '오류'; }
 }
